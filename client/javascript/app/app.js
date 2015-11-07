d9d3134099523bc2530379f47c55f36a88a7406b
Thom <me@thomrichardson.net>
2015-11-07 15:54:50 -0600

    Changed button styling

--- client/javascript/app/app.js
+++ client/javascript/app/app.js
@@ -123,3 +123,44 @@ app.config([
         $rootScope.current = toState.name.charAt(0).toUpperCase() + toState.name.slice(1);
     });
 }]);
+
+
+var dragSource;
+
+function dragLeave(div, event) {
+    div.style.opacity = 1;
+}
+function dragOver(div, event) {
+    div.style.opacity = 0.3;
+    event.preventDefault();
+}
+function dragStart(div, event) {
+    dragSource = div;
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', div.innerHTML);
+}
+function dragDrop(div, event) {
+    div.style.opacity = 1;
+    if (event.stopPropogation) {
+        event.stopPropogation();
+    }
+
+    if (dragSource != div) {
+        var oldId = dragSource.id;
+        dragSource.innerHTML = div.innerHTML;
+        dragSource.id = div.id;
+        div.id = oldId;
+        div.innerHTML = event.dataTransfer.getData('text/html');
+    }
+
+    return false;
+}
+function deleteBlock(div) {
+    var viewContent = '';
+    _.forEach(document.getElementById('viewEditor').childNodes, function(block) {
+        if (block.id !== div) {
+            viewContent += '{{-' + block.id + '}}\n';
+        }
+    });
+    angular.element($('#viewEditor')).scope().deleteBlock(viewContent);
+}
