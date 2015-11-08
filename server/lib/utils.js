137c93c268c939815207dac4508e2062a7cc48e5
Thom <me@thomrichardson.net>
2015-11-07 21:50:28 -0600

    Route fix

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,19 +1,82 @@
 var util = require('util');
 var config = require('../config/index');
+var Styles = require('../models/Style');
+var Scripts = require('../models/Scripts');
+var View = require('../models/View');
 var Uglify = require("uglify-js");
+var _ = require('lodash');
 
-module.exports = {
-    buildDbPath: function() {
-        return util.format('%s:%s/%s', config.dbServer, config.dbPort, config.dbDatabase);
-    },
-    obfuscateJs: function(code) {
-        var ast = Uglify.parse(code);
-        var compress = Uglify.Compressor();
-        ast.figure_out_scope();
-        ast = ast.transform(compress);
-        ast.figure_out_scope();
-        ast.compute_char_frequency();
-        ast.mangle_names();
-        return Uglify.minify(ast.print_to_string(), {fromString: true}).code
-    }
+var utils = {};
+
+utils.buildDbPath = function() {
+    return util.format('%s:%s/%s', config.dbServer, config.dbPort, config.dbDatabase);
 };
+
+utils.obfuscateJs = function(code) {
+    var ast = Uglify.parse(code);
+    var compress = Uglify.Compressor();
+    ast.figure_out_scope();
+    ast = ast.transform(compress);
+    ast.figure_out_scope();
+    ast.compute_char_frequency();
+    ast.mangle_names();
+    return Uglify.minify(ast.print_to_string(), {fromString: true}).code;
+};
+
+utils.buildPage = function(viewId) {
+    var bodyContent;
+
+    return Styles.getAllStyles().then(function(result) {
+        var styleContent = '<style>';
+        var linkContent = '';
+        _.each(result, function(style) {
+            if (style.enabled) {
+                var code = style.content;
+
+                if (style.type === 'code') {
+                    styleContent += code;
+                } else if (style.type === 'link') {
+                    linkContent += '<link rel="stylesheet" href="' + code + '">';
+                }
+            }
+        });
+        styleContent += '</style>';
+        return linkContent + styleContent;
+    }).then(function(styleContent) {
+        var content = {
+            styleContent: styleContent,
+            scriptsContent: ''
+        };
+        return Scripts.getAllScripts().then(function (result) {
+            var codeContent = '<script>';
+            var linkContent = '';
+            _.each(result, function (script) {
+                if (script.enabled) {
+                    var code = script.content;
+
+                    if (script.type === 'code') {
+                        codeContent += utils.obfuscateJs(code);
+                    } else if (script.type === 'link') {
+                        linkContent += '<script src="' + code + '"></script>';
+                    }
+                }
+            });
+
+            codeContent += '</script>';
+            content.scriptsContent = linkContent + codeContent;
+
+            return content;
+        });
+    }).then(function(content) {
+        return View.getView(viewId).then(function(result) {
+            bodyContent = result.body.content;
+            return {
+                bodyContent: bodyContent,
+                styleContent: content.styleContent.replace(/\n/g, ''),
+                scriptsContent: content.scriptsContent
+            };
+        });
+    });
+};
+
+module.exports = utils;
--- server/routes/index.js
+++ server/routes/index.js
@@ -14,82 +14,10 @@ router.get('/', function(req, res, next) {
 
     View.getDefaultViewId().then(function(result) {
         defaultViewId = result;
-        buildPage(defaultViewId).then(function(result){
+        utils.buildPage(defaultViewId).then(function(result){
             res.render('index', result);
         });
     });
 });
-
-// All custom defined routes by the user
-router.get('/*', function(req, res) {
-    View.getRoutes().then(function(result) {
-        var reqRoute = _.find(result, { 'route': req.url });
-        if (reqRoute) {
-            buildPage(reqRoute._id).then(function(result){
-                res.render('index', result);
-            });
-        } else {
-            res.render('index', {
-                bodyContent: '404 Route Not Found.'
-            });
-        }
-    });
-});
-
-function buildPage(viewId) {
-    var bodyContent;
-
-    return Styles.getAllStyles().then(function(result) {
-        var styleContent = '<style>';
-        var linkContent = '';
-        _.each(result, function(style) {
-            if (style.enabled) {
-                var code = style.content;
-
-                if (style.type === 'code') {
-                    styleContent += code;
-                } else if (style.type === 'link') {
-                    linkContent += '<link rel="stylesheet" href="' + code + '">';
-                }
-            }
-        });
-        styleContent += '</style>';
-        return linkContent + styleContent;
-    }).then(function(styleContent) {
-        var content = {
-            styleContent: styleContent,
-            scriptsContent: ''
-        };
-        return Scripts.getAllScripts().then(function (result) {
-            var codeContent = '<script>';
-            var linkContent = '';
-            _.each(result, function (script) {
-                if (script.enabled) {
-                    var code = script.content;
-
-                    if (script.type === 'code') {
-                        codeContent += utils.obfuscateJs(code);
-                    } else if (script.type === 'link') {
-                        linkContent += '<script src="' + code + '"></script>';
-                    }
-                }
-            });
-
-            codeContent += '</script>';
-            content.scriptsContent = linkContent + codeContent;
-
-            return content;
-        });
-    }).then(function(content) {
-        return View.getView(viewId).then(function(result) {
-            bodyContent = result.body.content;
-            return {
-                bodyContent: bodyContent,
-                styleContent: content.styleContent.replace(/\n/g, ''),
-                scriptsContent: content.scriptsContent
-            };
-        });
-    });
-}
 
 module.exports = router;
